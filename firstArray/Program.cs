efbce36420808640dbaba561b80d17a8ae4f9258
jeremy <projeremylegendre@gmail.com>
2023-11-07 16:43:39 +0100

    adde first forEach

--- firstArray/Program.cs
+++ firstArray/Program.cs
@@ -8,14 +8,10 @@ fraudulentOrderIDs[2] = "C789";
 */
 
 string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
-
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
 fraudulentOrderIDs[0] = "F000";
-
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
 // la proprité length n'est pas de base 0
---
+++ firstForEach/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] names = { "Rowena", "Robin", "Bao" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+int [] inventory = { 200,450,700,175,250};
+int sum =0;
+int bin =0;
+foreach(int item in inventory)
+{
+    bin++;
+   sum += item ;
+   Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+   
+}
+Console.WriteLine($"somme total {sum}");
