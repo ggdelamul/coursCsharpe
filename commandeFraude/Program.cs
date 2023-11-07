176699e386b73cd18456497f73d0cd107cf1d273
jeremy <projeremylegendre@gmail.com>
2023-11-07 16:56:58 +0100

    added commandeFraude

---
+++ commandeFraude/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Déclarez un tableau et initialisez-le pour qu’il contienne les éléments suivants :
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+Créez une instruction foreach pour parcourir chaque élément de votre tableau.
+Signalez les ID de commande qui commencent par la lettre « B »
+utilisez la méthode String.StartsWith()
+
+*/
+string [] numCommandes  = {"B123","C234","A345","C15","B177","G3003","C235", "B179"};
+
+foreach(string oneCommande in numCommandes )
+{
+    if(oneCommande.StartsWith("B"))
+    {
+        Console.WriteLine(oneCommande);
+    }
+}
